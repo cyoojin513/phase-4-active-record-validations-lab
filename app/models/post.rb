0e574e7cfd8862a10801088fa8cf1d6dfc9022b1
yoojin <yoojin9005@gmail.com>
2022-08-30 22:38:54 -0400

    Done except clickbait

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+  validates :clickbait?
+
+  def clickbait?
+    # "Won't Believe" || "Secret" || `Top #{Integer}` || "Guess"
+      errors.add(:title, "Matching title not found")
+  end
 end
